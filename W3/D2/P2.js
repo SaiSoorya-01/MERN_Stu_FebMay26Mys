b5a4da9ed78ceb4e8b721e67782960cb95162f10
SaiSoorya-01 <saisooryam@gmail.com>
2026-03-03 15:25:13 +0530

    Coparison operators

---
+++ W3/D2/P2.js
@@ -0,0 +1,12 @@
+//Coparison operators
+
+console.log("Coparison operators");
+let x=2,y='4';
+console.log("x==y:",x==y);//loose equality
+console.log("x===y:",x===y);//Strict equality
+console.log("x!=y:",x!=y);//loose inequality
+console.log("x!==y:",x!==y);//Strict inequality
+console.log("x>3",x>3);
+console.log("x<3", x<3);
+console.log("x>=3",x>=3);
+console.log("x<=3",x<=3);
