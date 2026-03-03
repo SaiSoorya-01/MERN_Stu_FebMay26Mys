fcf9b28a4db39fc8ff6fca6de7bd8f361e1ca730
SaiSoorya-01 <saisooryam@gmail.com>
2026-03-03 15:24:38 +0530

    Arthmetic Operator

---
+++ W3/D2/P1.js
@@ -0,0 +1,14 @@
+// Arthmetic Operator
+console.log("Arthmetic Operator");
+const a = 2, b = 3 ; 
+let c = 0;
+console.log("a+b",a+b);
+console.log("a-b",a-b);
+console.log("a*b",a*b);
+console.log("a/b",a/b);
+console.log("a%b",a%b);//Modules
+console.log("a**b",a**b);//Exponential
+// console.log("a++",a++);//post-increment
+// console.log("++a",++a);//post-increment
+// console.log("a--",a--);//post-decrement
+// console.log("--a",--a);//post-decrement
