3ab4ce888f61b2c9f99af2d7726a5efd741cc2ef
SaiSoorya-01 <saisooryam@gmail.com>
2026-03-03 15:42:57 +0530

    callback function

---
+++ W3/D2/P10.js
@@ -0,0 +1,10 @@
+//callback function
+// is a function which is passed as an argument to another function
+
+function greetUser(name,callback){
+    console.log("Hello,",name);
+    callback();
+}
+greetUser("soorya",function(){
+    console.log("callback function executed");
+});
