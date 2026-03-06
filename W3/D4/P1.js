52f6b4b08f478a52c0a27793a6cd3e998ff31f9e
SaiSoorya-01 <saisooryam@gmail.com>
2026-03-06 09:24:05 +0530

    Array Basics

---
+++ W3/D4/P1.js
@@ -0,0 +1,30 @@
+// Array Basics
+console.log("Array Basics")
+// Creating rrays
+let emptyArray = [];
+let numArray = [1,2,3,4];
+let mixedArray = [42,"hello",true,null,{name:"Gola"},[5,6]];
+console.log(emptyArray);
+console.log(numArray);
+console.log(mixedArray);
+
+//Using constructor
+let fruits = new Array("Apple","Mango");
+// console.log(fruits);
+console.log("Is fruits an array?",Array.isArray(fruits));
+
+// Push :add
+fruits.push("Cherry");
+// console.log(fruits);
+
+// pop :remove
+fruits.pop();
+// console.log(fruits);
+
+// unshift: adds to beginning
+fruits.unshift("Orange");
+// console.log(fruits);
+
+// shift: remove from beginning
+fruits.shift();
+console.log(fruits);
