544ecdef73fd5d7083a32e85745f744b463e2d70
SaiSoorya-01 <saisooryam@gmail.com>
2026-03-16 16:23:02 +0530

    Introduction to Node JS

---
+++ W5/D1/P1.js
@@ -0,0 +1,12 @@
+// Introduction to Node JS
+
+const runtimeName = "Node.js";
+// console.log("Introduction to Node.js");
+// console.log(`${runtimeName} runs javascript outside the browser`);
+
+// array desctructuring
+const commonUses = ["used for  server-side app","automation scripts can be created"];
+// console .log(commonUses[0]);
+// console .log(commonUses[1]);
+commonUses.forEach((commonUse,index) =>{console.log(`${index+1}.${commonUse}`);
+})
