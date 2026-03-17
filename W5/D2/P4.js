841cd8a85d384b5e4fc3880fef6c45ae21c87ac6
SaiSoorya-01 <saisooryam@gmail.com>
2026-03-17 13:30:11 +0530

    Writing Custom callback functions

---
+++ W5/D2/P4.js
@@ -0,0 +1,31 @@
+// Writing Custom callback functions
+
+function processStudent(name,score,callback){
+    console.log("Student name: ",name);
+    console.log("Score: ",score);
+
+    callback(name,score);
+}
+function showResult(name,score,callback){
+    if(score >= 70){
+        console.log(name+ "has passed.");
+    }else{
+        console.log(name+ "has failed.");
+    }
+}
+function showGrade(name,score,callback){
+    if(score >= 85){
+        console.log("Grade : A+");
+    }
+    else if(score >= 75){
+        console.log("Grade : A");
+    }
+    else if(score >= 70){
+        console.log("Grade : B");
+    }
+    else{
+        console.log("Grade : Fail")
+    }
+}
+processStudent("Baji rao",86,showGrade);
+processStudent("baji rao",86,showResult);
