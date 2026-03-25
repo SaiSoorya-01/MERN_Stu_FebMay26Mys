399760ce4fd4018547c9195a88a9af939e79fb87
SaiSoorya-01 <saisooryam@gmail.com>
2026-03-25 11:55:02 +0530

    Renaming ,deleting checking the file existence

---
+++ W6/D3/P5.js
@@ -0,0 +1,10 @@
+// Renaming ,deleting checking the file existence 
+
+const fs = require("fs");
+const path = require("path");
+
+const originalPath = path.join(__dirname,"draft-report.txt");
+const renamedPath = path.join(__dirname,"final-report.txt");
+
+fs.writeFileSync(originalPath,"Draft report Content");
+console.log("Does draft-report.txt exists?",fs.existsSync(originalPath));
