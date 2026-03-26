a3fdae76a854381285b877fd2225809035fcaf1c
SaiSoorya-01 <saisooryam@gmail.com>
2026-03-26 17:28:38 +0530

    Sending response in JSON format

---
+++ W6/D4/P6.js
@@ -0,0 +1,21 @@
+// Sending response in JSON format 
+
+const http = require("http");
+
+const server = http.createServer(function (req, res) {
+    if (req.url === "/api/status" && req.method === "GET") {
+        const responseData = {
+            success : true, message:"API is wroking",server:"NodeJS HTTP MOdule"
+        };
+        // JSON responses using application/json
+        res.writeHead(200,{"Content-type":"application/json"});
+        // JSON.Stringify() converts JS object into a JSON String 
+        res.end(JSON.stringify(responseData));
+        return;
+    }
+    res.writeHead(404,{"Content-type": "application/json"});
+    res.end(JSON.stringify({success : false,message:"Route not found."}));
+});
+server.listen(3001,function(){
+    console.log("Server is running at http://localhost:3001"); 
+});
