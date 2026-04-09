073d3671e9bfb5fcb8c8602f2efa6ffda0bc82f4
SaiSoorya-01 <saisooryam@gmail.com>
2026-04-09 12:07:58 +0530

    Generic error handler is created

--- Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/middleware/errorMiddleware.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/middleware/errorMiddleware.js
@@ -1,11 +1,11 @@
-// Generic error handler is creATED
+// Generic error handler is created
 
 function errorMiddleware(err,req,res,next){
     const statusCode = err.statusCode || 500;
 
     res.status(statusCode).json({
         success: false,
-        message: error.message || "Internal server error"
+        message: err.message || "Internal server error"
     });
 }
 
