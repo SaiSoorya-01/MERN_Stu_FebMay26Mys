7e338abb873b9b804c705d5a8cafdad8ef8d1924
SaiSoorya-01 <saisooryam@gmail.com>
2026-04-09 12:07:21 +0530

    Middleware to create and/or verify JWT token

--- Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/middleware/authMiddleware.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/middleware/authMiddleware.js
@@ -1,18 +1,18 @@
-// Middleware to create and/or verify JWT token
-const jwt =  require("jsonwebtoken");
-const CustomError = require("../utils/customError");
+//Middleware to create and/or verify JWT token
+const jwt = require("jsonwebtoken");
+const CustomError = require("../utils/CustomError");
 
-const JWT_SECRET = "TimbaSecret";
+const JWT_SECRET = "TumbaSecret";
 
-function authMiddleware(req,res,next){
+function authMiddleware(req,res,next) {
     try{
-        const authHeader = req.Headers.authorization;
+        const authHeader = req.headers.authorization;
         const tokenFromHeader = authHeader && authHeader.startsWith("Bearer")?authHeader.split(" ")[1]:null;
 
         const token = tokenFromHeader || req.cookies.token;
 
         if (!token) {
-            return next (new CustomError("Access denied.Token not provided",404));
+            return next(new CustomError("Access denied. Token not provided",401));
         }
 
         const decoded = jwt.verify(token,JWT_SECRET);
@@ -20,7 +20,7 @@ function authMiddleware(req,res,next){
         next();
     }
     catch(error){
-        next (new CustomError("Access denied.Token not provided",404));
+        next(new CustomError("Invalid or expired token",401));
     }
 }
 
