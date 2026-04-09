5106a044bcd238fb3d89ed376052498f3d1e486b
SaiSoorya-01 <saisooryam@gmail.com>
2026-04-09 12:09:27 +0530

    Checks the permission for the req and allows it or rejects it

--- Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/middleware/roleMiddleware.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/middleware/roleMiddleware.js
@@ -1,13 +1,13 @@
-// Checks the permission for the req and allows it or rejects it
-const CustomError = require("../utils/customError");
+//Checks the permission for the req and allows it or rejects it
+const CustomError = require("../utils/CustomError");
 
 function roleMiddleware(...allowedRoles){
     return(req,res,next)=>{
         if (!req.user) {
-            return next (new CustomError("User info not found.",404));
+            return next(new CustomError("User info not found.",401));
         }
         if (!allowedRoles.includes(req.user.role)) {
-            return next (new CustomError("Forbidden: you do not have access to this resource.",404));
+            return next(new CustomError("Forbidden: You do not have access to this resource.",401));
         }
         next();
     };
