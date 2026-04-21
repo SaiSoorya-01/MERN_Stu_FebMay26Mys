fd7cd8dc39659258e212efde0617093b77bb1d0f
SaiSoorya-01 <saisooryam@gmail.com>
2026-04-21 09:37:15 +0530

    LMS

--- Team Project/CLI-based Learning Management System (LMS)/MERN_Week1_V1.0/courses.js
+++ Team Project/CLI-based Learning Management System (LMS)/MERN_Week1_V1.0/courses.js
@@ -1,46 +1,47 @@
-const courses = [{
-    id:1,
-    title:"MERN stack",
-    instructor:"Rakesh",
-    category:"Programming",
-    level:"Beginner",
-    sataus:"True",
-    totalNumber:"3",
-    lessons:["React.js","Express.js","Node.js"],
-    
-},
-{
-    id:2,
-    title:"Node.js",
-    instructor:"Sonu",
-    category:"Backend",
-    level:"Intermediate",
-    sataus:"False",
-    totalNumber:"4",
-    lessons:["Modules","Buffer","Events","File System"]
-},
-{
-    id:3,
-    title:"HTML & CSS",
-    instructor:"Anu",
-    category:"Web Development",
-    level:"Beginner",
-    sataus:"True",
-    totalNumber:"4",
-    lessons:["HTML Tags","CSS Basics","Grid","Flexbox"]
-},
-{
-    id:4,
-    title:"Python",
-    instructor:"Saara",
-    category:"Programming",
-    level:"Beginner",
-    sataus:"False",
-    totalNumber:"3",
-    lessons:["Syntax","Loops","Functions"]
-}
-];
+const courses = [
+  {
+    id: 1,
+    title: "MERN stack",
+    instructor: "Rakesh",
+    category: "Programming",
+    level: "Beginner",
+    status: true,
+    totalNumber: 3,
+    lessons: ["React.js", "Express.js", "Node.js"]
+  },
 
-// console.log(courses);
+  {
+    id: 2,
+    title: "Node.js",
+    instructor: "Sonu",
+    category: "Backend",
+    level: "Intermediate",
+    status: false,
+    totalNumber: 4,
+    lessons: ["Modules", "Buffer", "Events", "File System"]
+  },
+
+  {
+    id: 3,
+    title: "HTML & CSS",
+    instructor: "Anu",
+    category: "Web Development",
+    level: "Beginner",
+    status: true,
+    totalNumber: 4,
+    lessons: ["HTML Tags", "CSS Basics", "Grid", "Flexbox"]
+  },
+
+  {
+    id: 4,
+    title: "Python",
+    instructor: "Saara",
+    category: "Programming",
+    level: "Beginner",
+    status: false,
+    totalNumber: 3,
+    lessons: ["Syntax", "Loops", "Functions"]
+  }
+];
 
 module.exports = courses;
--- Team Project/CLI-based Learning Management System (LMS)/MERN_Week1_V1.0/validator.js
+++ Team Project/CLI-based Learning Management System (LMS)/MERN_Week1_V1.0/validator.js
@@ -1,8 +1,17 @@
-function validateChoice(input, callback) {
-  if (!input || isNaN(input)) {
-    return callback("Invalid input", null);
+let attempts = 0;
+
+function validateInput(input, callback) {
+  if (!input) {
+    attempts++;
+    if (attempts >= 3) {
+      callback("Max attempts reached", null);
+    } else {
+      callback("Invalid input, try again", null);
+    }
+  } else {
+    attempts = 0;
+    callback(null, input);
   }
-  callback(null, Number(input));
 }
 
-module.exports = { validateChoice };
+module.exports = validateInput;
